4b6b8edaefbbbdf8e13c1222560df10d26c79da2
m-sarabi <m.sarabi.jkd@gmail.com>
2024-04-21 22:49:49 +0330

    better anti afk and a good smooth look function

--- anti_afk/random_look.js
+++ anti_afk/random_look.js
@@ -1,32 +1,33 @@
-const player = Player.getPlayer();
-var start = true;
-var velocity = [0.0, 0.0];
-var counter = 0;
-while (true) {
-    Client.waitTick(Math.floor(Math.random() * 1200 + 600));
-    while (player.hasStatusEffect("weakness") || counter > 0) {
-        if (start === true) {
-            Chat.log("Weakness is your weakness!!");
-            counter = Math.floor(500);
-            start = false;
-            KeyBind.keyBind("key.sneak", true);
-            KeyBind.keyBind("key.forward", true);
-        }
-        counter -= 1;
-        var rotation = [player.getYaw(), player.getPitch()];
-        velocity[0] = Math.max(-2, Math.min(2, rotation[0] + (Math.random() - 0.5) * 0.1));
-        //velocity[1] = Math.max(-1, Math.min(1, rotation[1] + (Math.random() - 0.5) * 0.01));
-        rotation[0] = rotation[0] + velocity[0];
-        if (rotation[0] > 180) {
-            rotation[0] = rotation[0] - 360;
-        } else if (rotation[0] < -180) {
-            rotation[0] = 360 + rotation[0];
-        }
-        //rotation[1] = rotation[1] + velocity[1];
-        player.lookAt(rotation[0], rotation[1]);
-        Time.sleep(20);
+function smooth_look(yaw, pitch, steps = 60, duration = 300) {
+    var old_yaw = Player.getPlayer().getYaw();
+    var old_pitch = Player.getPlayer().getPitch();
+
+    if (yaw < old_yaw) {
+        yaw = yaw + 360;
     }
-    start = true;
-    KeyBind.keyBind("key.forward", false);
-    KeyBind.keyBind("key.sneak", false);
+
+    if (yaw - old_yaw > 180) {
+        var positive = false;
+    } else {
+        var positive = true;
+    }
+    if (positive) {
+        for (var i = 0; i < steps; i++) {
+            Player.getPlayer().lookAt(old_yaw + (yaw - old_yaw) / steps * (i + 1),
+                old_pitch + (pitch - old_pitch) / steps * (i + 1));
+            Time.sleep(Math.floor(duration / steps));
+        }
+    } else {
+        for (var i = 0; i < steps; i++) {
+            Player.getPlayer().lookAt(old_yaw - (360 + old_yaw - yaw) / steps * (i + 1),
+                old_pitch + (pitch - old_pitch) / steps * (i + 1));
+            Time.sleep(Math.floor(duration / steps));
+        }
+    }
+}
+
+// check if player has weakness
+while (Player.getPlayer().hasStatusEffect("weakness")) {
+    smooth_look(Math.random() * 360, Math.random() * 170 - 85);
+    Client.waitTick(Math.floor(Math.random() * 600 + 300));
 }

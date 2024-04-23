e02b27ffa124540b0117033082cde183cb587f7e
m-sarabi <m.sarabi.jkd@gmail.com>
2024-04-23 07:41:58 +0330

    changed the keybind to interact

--- anti_afk/mcmmo_fish_helper.js
+++ anti_afk/mcmmo_fish_helper.js
@@ -19,20 +19,20 @@ function smooth_look(old_yaw, new_yaw, old_pitch, new_pitch, steps = 20, duratio
     };
 }
 
-// if that area is depleted of fish, look 90 degrees to the other side
-var randomness = 6;
-JsMacros.on("RecvMessage", false, JavaWrapper.methodToJava((event) => {
-    var yaw = Player.getPlayer().getYaw();
-    var pitch = Player.getPlayer().getPitch();
-    var text = event.text.getString().toLowerCase();
-    if (text.includes("you sense that")) {
-        if (yaw >= -15 && yaw <= 15) {
-            smooth_look(yaw, randomness * (Math.random() - 0.5) + 90, pitch, pitch);
-        } else if (yaw >= 75 && yaw <= 105) {
-            smooth_look(yaw, randomness * (Math.random() - 0.5), pitch, pitch);
-        }
-    }
-}));
+// // if that area is depleted of fish, look 90 degrees to the other side
+// var randomness = 6;
+// JsMacros.on("RecvMessage", false, JavaWrapper.methodToJava((event) => {
+//     var yaw = Player.getPlayer().getYaw();
+//     var pitch = Player.getPlayer().getPitch();
+//     var text = event.text.getString().toLowerCase();
+//     if (text.includes("you sense that")) {
+//         if (yaw >= -15 && yaw <= 15) {
+//             smooth_look(yaw, randomness * (Math.random() - 0.5) + 90, pitch, pitch);
+//         } else if (yaw >= 75 && yaw <= 105) {
+//             smooth_look(yaw, randomness * (Math.random() - 0.5), pitch, pitch);
+//         }
+//     }
+// }));
 
 // catch and recast fishes while also rotating the head
 JsMacros.on("Sound", false, JavaWrapper.methodToJava((event) => {
@@ -40,9 +40,7 @@ JsMacros.on("Sound", false, JavaWrapper.methodToJava((event) => {
         (Player.getPlayer().getMainHand().getItemId() == "minecraft:fishing_rod" ||
             Player.getPlayer().getOffHand().getItemId() == "minecraft:fishing_rod")) {
         Time.sleep(Math.floor(Math.random() * 150) + 150);
-        KeyBind.keyBind("key.use", true);
-        Time.sleep(Math.floor(Math.random() * 40) + 30);
-        KeyBind.keyBind("key.use", false);
+        Player.interactions().interact();
 
         // recast only if holding a fishing rod
         Time.sleep(Math.floor(Math.random() * 200) + 200);
@@ -53,8 +51,9 @@ JsMacros.on("Sound", false, JavaWrapper.methodToJava((event) => {
         );
         Time.sleep(Math.floor(Math.random() * 200) + 200);
 
-        KeyBind.keyBind("key.use", true);
-        Time.sleep(Math.floor(Math.random() * 40) + 30);
-        KeyBind.keyBind("key.use", false);
+        Player.interactions().interact();
     }
 }));
+
+// mods:
+// Seraphiael

86602e0568cda28cc073284c6ad946429d159d35
m-sarabi <m.sarabi.jkd@gmail.com>
2024-05-02 04:14:55 +0330

    removed unused files

--- farming/detect_grown.js
+++
@@ -1,7 +0,0 @@
-while (true) {
-    if (Player.rayTraceBlock(3, false).getBlockState().age == 3) {
-        break;
-    }
-    Client.waitTick(10);
-}
-World.playSound("minecraft:entity.ender_dragon.death", 100, 1.5);
